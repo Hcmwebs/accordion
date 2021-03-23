611d83b8c41d141f207cab71b2e884c648c4541a
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-03-24 11:15:52 +1300

    fixed the margin on the h1. Tried to make script more dynamic not so successfully

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 const btn = document.querySelectorAll('button');
 const para = document.querySelectorAll('.para');
 
-current  = 0
+
 
 for(let i = 0; i <btn.length; i++) {
   btn[i].addEventListener('click', () => {
-    btn[i].classList.toggle('active');
-    para[i].classList.toggle('active');
+
+        btn[i].classList.toggle('active');
+        para[i].classList.toggle('active');
+
+
+
+
   })
 
 }
